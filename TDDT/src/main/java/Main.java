65598c028ffcc55eeaf033e29d290d1b0d665b33
Meerer <Stefan.Kerren@googlemail.com>
2016-06-26 18:07:43 +0200

    XML_3
    f

--- TDDT/src/main/java/Main.java
+++ TDDT/src/main/java/Main.java
@@ -3,12 +3,18 @@ import XML_body.*;
 /**
  * Created by Andreas on 24.06.2016.
  */
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
 
         System.out.println("Hello Tuck Festing!");
         Exersises e = new Exersises();
+        Exersise t = new Exersise();
+        t.setExersiseName("test");
+        ArrayList<Exersise> tl = new ArrayList<Exersise>();
+        tl.add(t);
+        e.setExersises(tl);
        // System.out.println(e.exersises.get(0).exersiseName);
         XMLController.saveAllExercises(e);
     }
